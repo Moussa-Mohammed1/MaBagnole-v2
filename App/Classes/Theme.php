be4b184565bd104229add4b3ad78f45746d9238d
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-01-08 15:25:54 +0100

    feat(theme): Add theme class with CRUD operations

---
+++ App/Classes/Theme.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Classes;
+
+use PDO;
+use App\Config\Database;
+
+class Theme
+{
+    private $id_theme;
+    private $titre;
+    private $image;
+
+    public function __construct(string $titre, string $image)
+    {
+        $this->titre = $titre;
+        $this->image = $image;
+    }
+
+    public function addTheme() : void
+    {
+        $pdo = Database::getInstance()->getConnection();
+        $sql = 'INSERT INTO theme(titre,image)
+                VALUES (?,?)';
+        $st = $pdo->prepare($sql);
+        $st->execute([$this->titre, $this->image]);
+    }
+    public function updateTheme(int $id_theme, string $titre, string $image)
+    {
+        $pdo = Database::getInstance()->getConnection();
+        $sql = 'UPDATE theme SET titre = ?, image = ? WHERE id_theme = ?';
+        $st = $pdo->prepare($sql);
+        $st->execute([$titre, $image, $id_theme]);
+    }
+
+    public function deleteTheme(int $id_theme) : void
+    {
+        $pdo = Database::getInstance()->getConnection();
+        $sql = 'DELETE FROM theme WHERE id_theme = ?';
+        $st = $pdo->prepare($sql);
+        $st->execute([$id_theme]);
+    }
+
+    public static function getAllThemes() : ?array {
+        $pdo = Database::getInstance()->getConnection();
+        $sql = 'SELECT  t.image, t.id_theme, 
+                        t.titre, COUNT(a.id_theme) 
+                FROM theme t
+                LEFT JOIN article a 
+                ON t.id_theme = a.id_theme
+                GROUP BY t.image, t.id_theme, t.titre';
+        $st = $pdo->prepare($sql);
+        return $st->execute() ? $st->fetchAll(PDO::FETCH_OBJ) : null;
+    }
+}
